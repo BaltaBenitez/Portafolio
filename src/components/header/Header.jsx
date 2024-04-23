87750b47eb661b7d01c306aa31db369a96092a19
baltasar benitez <baltaarex363@gmail.com>
2024-04-22 21:26:41 -0300

    responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () =>{
     return(
         <header className="mb-2 sticky-top">
-            <div className="container-fluid bg-dark">
+            <div className="container-fluid bg-dark sticky-top">
                 <div className="row">
                     <div className="col d-flex justify-content-center">
                         <a href="#proyectos" className="nav-link p-3 text-light">PROYECTOS</a>
--- src/components/main/Proyects.jsx
+++ src/components/main/Proyects.jsx
@@ -5,17 +5,18 @@ const Proyects = () => {
     const arenaURl = 'https://arenasportgym.netlify.app/'
     const arenaTitle = 'arena sport gym'
     const arenaGit = 'https://github.com/BaltaBenitez/mi-repo.git'
+
     const redBullURl = 'https://rb-20.netlify.app/'
     const redbullTitle = 'red bull online shop'
     const redbullGit = 'https://github.com/BaltaBenitez/49970-Rjs.git'
 
     return(
-        <div id='proyectos' className="container my-5">
+        <div id='proyectos' className="container">
             <h2><i className="bi bi-code-slash"></i> Proyectos</h2>
             <div className="row">
-                <div className="col my-3 d-md-flex flex-column flex-sm-row justify-content-center justify-content-md-center align-items-center">
+                <div className="col d-md-flex flex-column flex-sm-row justify-content-center justify-content-md-center align-items-center">
                     <div className='text-center'>
-                        <a href={arenaURl} target="_blank" rel="noopener noreferrer"><img src={pageArena} alt="" width={400}/></a>
+                        <a href={arenaURl} target="_blank" rel="noopener noreferrer"><img src={pageArena} alt=""width={350}/></a>
                     </div>
                     <div className='mx-5 my-4'>
                         <h3><strong>{arenaTitle.toUpperCase()}</strong></h3>
@@ -23,9 +24,9 @@ const Proyects = () => {
                         <p className='btn btn-outline-dark'><a href={arenaGit} target="_blank" rel="noopener noreferrer" className='text-decoration-none text-light'>Más información en <i className="bi bi-github"></i></a></p>
                     </div>
                 </div>
-                <div className="col my-3 d-md-flex flex-column flex-sm-row justify-content-center justify-content-md-center align-items-center">
+                <div className="col d-md-flex flex-column flex-sm-row justify-content-center justify-content-md-center align-items-center">
                     <div className='text-center'>
-                        <a href={redBullURl} target="_blank" rel="noopener noreferrer"><img src={pageRedBull} alt="" width={400}/></a>
+                        <a href={redBullURl} target="_blank" rel="noopener noreferrer"><img src={pageRedBull} alt="" width={350}/></a>
                     </div>
                     <div className='mx-5 my-4'>
                         <h3><strong>{redbullTitle.toUpperCase()}</strong></h3>

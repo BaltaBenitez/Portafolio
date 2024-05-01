4e4ee708a0daa94e6cb6d65398815a550e477519
baltasar benitez <baltaarex363@gmail.com>
2024-04-30 22:50:13 -0300

    footer

--- src/components/footer/Contac.jsx
+++ src/components/footer/Contac.jsx
@@ -1,13 +1,11 @@
 const Contac = ( {redesLink} ) => {
     const year = new Date().getFullYear()
-    const miduDev = 'https://porfolio.dev/'
-    /* <p>Este proyecto fue inspirado en el repositorio de <a href={miduDev} target="_blank" rel="noopener noreferrer" className="text-decoration-none text-warning fst-italic"> MiduDev.</a></p> */
 
     return(
-        <footer id='contacto' className="container text-center mt-4">
-            <div className="row">
+        <footer id='contacto' className="container text-center mt-4 px-4">
+            <div className="d-sm-flex flex-column flex-sm-row justify-content-between">
                 <h6>@ {year} - Baltasar Benitez</h6>
-                <p id="ul__footer" className="list-unstyled d-flex justify-content-center">
+                <p id="ul__footer" className="list-unstyled">
                     <a href={redesLink.gitHub} target="_blank" rel="noopener noreferrer" className="px-2"><i className="bi bi-github"></i></a>
                     <a href={redesLink.correo} target="_blank" rel="noopener noreferrer" className="px-2"><i className="bi bi-envelope-at"></i></a>
                     <a href={redesLink.linkedIn} target="_blank" rel="noopener noreferrer" className="px-2"><i className="bi bi-linkedin"></i></a>
